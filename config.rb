5491dfec625b11a921b724c9fd4d1be8eb768194
Alice Fabre <alice.fabre@hotmail.fr>
2019-11-07 15:26:28 +0100

    correction config pour déploiement

--- config.rb
+++ config.rb
@@ -6,6 +6,7 @@ configure :build do
 end
 
 set :relative_links, true
+set :relative_assets, true
 
 # Notre gem installée, il faut maintenant la configurer. Middleman-deploy offre plusieurs possibilités de déploiement via ftp, sftp, rsync ou git. On doit donc lui indiquer quelle méthode on choisit. On va cette fois ouvrir le fichier config.rb et ajouter ce qui suit avant le bloc configure:
 activate :deploy do |deploy|
