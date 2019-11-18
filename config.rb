fea7696eff9d714f03bc52274548c8f247ad8aba
Alice Fabre <alice.fabre@hotmail.fr>
2019-11-18 20:32:40 +0100

    favicon réseaux sociaux

--- config.rb
+++ config.rb
@@ -24,7 +24,7 @@ configure :build do
     f.template_dir  = File.join(root, 'source')
     f.output_dir    = File.join(root, 'build')
     f.icons = {
-      "_favicon_template.png" => [
+      "favicon_template.png" => [
         { icon: "apple-touch-icon-152x152-precomposed.png" },
         { icon: "apple-touch-icon-144x144-precomposed.png" },
         { icon: "apple-touch-icon-120x120-precomposed.png" },
@@ -42,7 +42,7 @@ configure :build do
         { icon: "favicon-16x16.png" },
         { icon: "favicon.png", size: "16x16" },
         { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },
-        { icon: "mstile-144x144", format: "png" },
+        { icon: "mstile-144x144", format: "png" }
       ]
     }
   end
