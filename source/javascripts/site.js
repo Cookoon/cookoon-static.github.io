b3d4ea70e0261748ba252752b8c5c5c2da37d29d
Alice Fabre <alice.fabre@hotmail.fr>
2019-11-26 18:49:35 +0100

    onglet menu bold si on est dans la bonne page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,2 +1,13 @@
-// This is where it all goes :)
+const path = window.location.pathname.split("/").pop();
+
+
+document.querySelectorAll("menuTab").forEach((tab) => {
+    tab.classList.remove("active");
+  });
+
+if (path == "occasions-particulieres") {
+  document.getElementById("particulier").classList.add("active");
+} else if (path == "receptions-professionnelles") {
+  document.getElementById("professionnel").classList.add("active");
+}
 
