391021e581b1be28f8b72a4bb1b09479a4c6d5a2
Alice Fabre <alice.fabre@hotmail.fr>
2019-11-27 14:00:24 +0100

    test javascript

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,13 +1,17 @@
-const path = window.location.pathname.split("/").pop();
+
+$(document).ready(function() {
+  const path = window.location.pathname.split("/").pop();
+
+  document.querySelectorAll("menuTab").forEach((tab) => {
+      tab.classList.remove("active");
+    });
+
+  if (path == "occasions-particulieres") {
+    document.getElementById("particulier").classList.add("active");
+  } else if (path == "receptions-professionnelles") {
+    document.getElementById("professionnel").classList.add("active");
+  }
+});
 
 
-document.querySelectorAll("menuTab").forEach((tab) => {
-    tab.classList.remove("active");
-  });
-
-if (path == "occasions-particulieres") {
-  document.getElementById("particulier").classList.add("active");
-} else if (path == "receptions-professionnelles") {
-  document.getElementById("professionnel").classList.add("active");
-}
 
