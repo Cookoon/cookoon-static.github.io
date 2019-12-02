adc0f4e7b90fdf21a9bf130406c547b2ce990dee
Alice Fabre <alice.fabre@hotmail.fr>
2019-12-02 18:43:44 +0100

    js prod

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -12,18 +12,18 @@
 //   }
 // });
 
-// $(document).ready(function() {
-//   var path = window.location.pathname.split("/").pop();
 
-//   document.querySelectorAll("menuTab").forEach(function(tab) {
-//       tab.classList.remove("active");
-//     });
+  var path = window.location.pathname.split("/").pop();
 
-//   if (path == "occasions-particulieres") {
-//     document.getElementById("particulier").classList.add("active");
-//   } else if (path == "receptions-professionnelles") {
-//     document.getElementById("professionnel").classList.add("active");
-//   }
-// });
+  document.querySelectorAll("menuTab").forEach(function(tab) {
+      tab.classList.remove("active");
+    });
+
+  if (path == "occasions-particulieres") {
+    document.getElementById("particulier").classList.add("active");
+  } else if (path == "receptions-professionnelles") {
+    document.getElementById("professionnel").classList.add("active");
+  }
+
 
 console.log("Alice")
